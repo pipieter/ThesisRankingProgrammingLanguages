16bc81cfd5ab36ecbf4eb98d7c74f977a5b769bd
Pim Pieters <pim.pieters@ugent.be>
2024-12-15 22:10:27 +0100

    add cores to to_csv

--- Scripts/to_csv.py
+++ Scripts/to_csv.py
@@ -1,4 +1,3 @@
-import dataclasses
 import json
 import os
 import statistics
@@ -9,78 +8,111 @@ from Scripts.measure import get_files
 ROOT = os.getcwd()
 
 
-@dataclasses.dataclass
 class EnergyData:
     energy: float
 
+    def __init__(self, path: str):
+        energy_samples = []
+        with open(path, "r") as file:
+            for line in file.readlines():
+                data = json.loads(line)
+                samples = data["energy_samples"]
+                energy = self._parse_samples(samples)
+                energy_samples.append(energy)
 
-@dataclasses.dataclass
+        self.energy = 0
+        if len(energy_samples) > 0:
+            self.energy = statistics.mean(energy_samples)
+
+    def _parse_samples(self, samples: list[any]) -> float:
+        energy = 0
+        for sample in samples:
+            for subsample in sample["energy"]:
+                energy += (
+                    subsample["pkg"]
+                    + subsample["pp0"]
+                    + subsample["pp1"]
+                    + subsample["dram"]
+                )
+        return energy
+
+
 class ResourceData:
     runtime: float
     avg_shared_memory: float
     avg_private_memory: float
+    avg_cores: float
 
-
-def get_energy_data(energy_file_path: str) -> EnergyData:
-    with open(energy_file_path, "r") as file:
-        energy_samples = []
-        for line in file.readlines():
-            data = json.loads(line)
-            samples = data["energy_samples"]
-            energy = 0
-            for sample in samples:
-                for subsample in sample["energy"]:
-                    energy += (
-                        subsample["pkg"]
-                        + subsample["pp0"]
-                        + subsample["pp1"]
-                        + subsample["dram"]
-                    )
-            energy_samples.append(energy)
-
-    energy = 0
-    if len(energy_samples) > 0:
-        energy = statistics.mean(energy_samples)
-
-    return EnergyData(energy=energy)
-
-
-def get_resources_data(resources_file_path: str) -> ResourceData:
-    with open(resources_file_path, "r") as file:
+    def __init__(self, path: str) -> None:
         runtimes = []
         avg_shared_memories = []
         avg_private_memories = []
+        avg_cores_used = []
 
-        for line in file.readlines():
-            data = json.loads(line)
-            runtime = data["total_runtime_ms"] / 1000  # In seconds
+        with open(path, "r") as file:
+            for line in file.readlines():
+                data = json.loads(line)
 
-            shared_memory = 0
-            private_memory = 0
-            for mem in data["samples"]:
-                # In seconds
-                shared_memory += mem["shared_memory"] * mem["runtime_ms"] / 1000
-                private_memory += mem["private_memory"] * mem["runtime_ms"] / 1000
+                # Ignore executions without any samples
+                if len(data["samples"]) == 0:
+                    continue
 
-            avg_shared_memory = shared_memory / runtime
-            avg_private_memory = private_memory / runtime
+                total_runtime = data["total_runtime_ms"] / 1000  # In seconds
+                samples = data["samples"]
 
-            runtimes.append(runtime)
-            avg_shared_memories.append(avg_shared_memory)
-            avg_private_memories.append(avg_private_memory)
+                parsed = self._parse_samples(samples)
+                if parsed is None:
+                    continue
 
-    if len(runtimes) == 0:
-        return ResourceData(0, 0, 0)
+                (
+                    avg_core_seconds,
+                    avg_shared_mem_seconds,
+                    avg_private_mem_seconds,
+                ) = parsed
 
-    runtime = statistics.mean(runtimes)
-    avg_shared_memory = statistics.mean(avg_shared_memories)
-    avg_private_memory = statistics.mean(avg_private_memories)
+                runtimes.append(total_runtime)
+                avg_cores_used.append(avg_core_seconds)
+                avg_shared_memories.append(avg_shared_mem_seconds)
+                avg_private_memories.append(avg_private_mem_seconds)
 
-    return ResourceData(
-        runtime=runtime,
-        avg_shared_memory=avg_shared_memory,
-        avg_private_memory=avg_private_memory,
-    )
+        self.runtime = 0
+        self.avg_cores = 0
+        self.avg_shared_memory = 0
+        self.avg_private_memory = 0
+
+        if len(runtimes) > 0:
+            self.runtime = statistics.mean(runtimes)
+            self.avg_cores = statistics.mean(avg_cores_used)
+            self.avg_shared_memory = statistics.mean(avg_shared_memories)
+            self.avg_private_memory = statistics.mean(avg_private_memories)
+
+    def _parse_samples(self, samples: list[any]) -> None | tuple[float, float, float]:
+        if len(samples) == 0:
+            return None
+
+        total_core_seconds = 0
+        total_shared_mem_seconds = 0
+        total_private_memory_seconds = 0
+        total_recorded_runtime_seconds = 0
+
+        for sample in samples:
+            sample_seconds = sample["runtime_ms"] / 1000
+
+            total_core_seconds += sample["cores"] * sample_seconds
+            total_shared_mem_seconds += sample["shared_memory"] * sample_seconds
+            total_private_memory_seconds += sample["private_memory"] * sample_seconds
+
+            total_recorded_runtime_seconds += sample_seconds
+
+        avg_core_seconds = total_core_seconds / total_recorded_runtime_seconds
+        avg_shared_mem_seconds = (
+            total_shared_mem_seconds / total_recorded_runtime_seconds
+        )
+        avg_private_mem_seconds = (
+            total_private_memory_seconds / total_recorded_runtime_seconds
+        )
+
+        return avg_core_seconds, avg_shared_mem_seconds, avg_private_mem_seconds
 
 
 class BenchmarkData:
@@ -102,8 +134,8 @@ class BenchmarkData:
         energy_path = os.path.join(ROOT, "Results", energy_file)
         resources_path = os.path.join(ROOT, "Results", resources_file)
 
-        self.energy = get_energy_data(energy_path)
-        self.resources = get_resources_data(resources_path)
+        self.energy = EnergyData(energy_path)
+        self.resources = ResourceData(resources_path)
 
     def __lt__(self, other) -> bool:
         if not isinstance(other, BenchmarkData):
@@ -113,19 +145,50 @@ class BenchmarkData:
             return self.benchmark < other.benchmark
 
         if self.identifier != other.identifier:
-            return self.benchmark < other.identifier
+            # Check if value can be an integer
+            try:
+                self_int = int(self.identifier, base=10)
+                other_int = int(other.identifier, base=10)
+                return self_int < other_int
+            except:
+                return self.identifier < other.identifier
 
         return self.language < other.language
 
     def to_csv_line(self, separator: str = ";") -> str:
-        return f"{self.benchmark}{separator}{self.identifier}{separator}{self.language}{separator}{self.energy.energy:.4f}{separator}{self.resources.runtime:.4f}{separator}{self.resources.avg_private_memory:.4f}{separator}{self.resources.avg_shared_memory:.4f}"
+        avg_total_memory = (
+            self.resources.avg_shared_memory + self.resources.avg_private_memory
+        )
+        values = [
+            self.benchmark,
+            self.identifier,
+            self.language,
+            f"{self.resources.runtime:.4f}",
+            f"{self.energy.energy:.4f}",
+            f"{self.resources.avg_cores:.4f}",
+            f"{avg_total_memory:.4f}",
+            f"{self.resources.avg_shared_memory:.4f}",
+            f"{self.resources.avg_private_memory:.4f}",
+        ]
+        return separator.join(values)
 
     @staticmethod
     def csv_header(separator: str = ";") -> str:
-        return f"Benchmark{separator}Identifier{separator}Language{separator}Energy (Ws){separator}Runtime (s){separator}Average private memory (KB){separator}Average shared memory (KB)"
+        headers = [
+            "Benchmark",
+            "Identifier",
+            "Language",
+            "Runtime (s)",
+            "Energy (Ws)",
+            "Average cores used",
+            "Average total memory (KB)",
+            "Average shared memory (KB)",
+            "Average private memory (KB)",
+        ]
+        return separator.join(headers)
 
 
-def parse():
+if __name__ == "__main__":
     path = os.path.join(ROOT, "Results")
 
     files = get_files(path)
@@ -142,7 +205,3 @@ def parse():
     print(BenchmarkData.csv_header())
     for d in data:
         print(d.to_csv_line())
-
-
-if __name__ == "__main__":
-    parse()
