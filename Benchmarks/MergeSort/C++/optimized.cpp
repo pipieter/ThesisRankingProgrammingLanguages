6e56c31adcea365a64093478fcd5b33b63a50b17
Pim Pieters <pim.pieters@ugent.be>
2025-01-13 19:05:59 +0100

    Add multithreading to C++ merge sort optimized

--- Benchmarks/MergeSort/C++/optimized.cpp
+++ Benchmarks/MergeSort/C++/optimized.cpp
@@ -1,14 +1,18 @@
 #include <cstring>
 #include <fstream>
 #include <string>
+#include <thread>
 #include <vector>
 
 using namespace std;
 
-vector<const char *> merge(const char **a_start, const char **a_end,
-                           const char **b_start, const char **b_end) {
-  size_t size = (a_end - a_start) + (b_end - b_start);
-  vector<const char *> merged(size);
+const long int CONCURRENT_THRESHOLD = 10000;
+
+void merge_sort_parallel(const char **start, const char **end);
+void merge_sort_concurrent(const char **start, const char **end);
+
+void merge(const char **target, const char **a_start, const char **a_end,
+           const char **b_start, const char **b_end) {
   size_t i = 0;
 
   const char **a = a_start;
@@ -16,42 +20,66 @@ vector<const char *> merge(const char **a_start, const char **a_end,
 
   while (a != a_end && b != b_end) {
     if (strcmp(*a, *b) < 0) {
-      merged[i] = *a;
+      target[i] = *a;
       a++;
     } else {
-      merged[i] = *b;
+      target[i] = *b;
       b++;
     }
     i++;
   }
 
   while (a != a_end) {
-    merged[i] = *a;
+    target[i] = *a;
     a++;
     i++;
   }
   while (b != b_end) {
-    merged[i] = *b;
+    target[i] = *b;
     b++;
     i++;
   }
-
-  return merged;
 }
 
-vector<const char *> merge_sort(const char **start, const char **end) {
+void merge_sort_parallel(const char **start, const char **end) {
   if ((end - start) <= 1) {
-    return vector<const char *>(start, end);
+    return;
+  }
+
+  if ((end - start) < CONCURRENT_THRESHOLD) {
+    merge_sort_concurrent(start, end);
+    return;
   }
 
   size_t half = (end - start) / 2;
-  size_t diff = end - start;
 
-  vector<const char *> left_sorted = merge_sort(start, start + half);
-  vector<const char *> right_sorted = merge_sort(start + half, end);
+  vector<const char *> left(start, start + half);
+  vector<const char *> right(start + half, end);
 
-  return merge(left_sorted.data(), left_sorted.data() + left_sorted.size(),
-               right_sorted.data(), right_sorted.data() + right_sorted.size());
+  std::thread left_thread(
+      [&left] { merge_sort_parallel(left.data(), left.data() + left.size()); });
+  merge_sort_parallel(right.data(), right.data() + right.size());
+  left_thread.join();
+
+  merge(start, left.data(), left.data() + left.size(), right.data(),
+        right.data() + right.size());
+}
+
+void merge_sort_concurrent(const char **start, const char **end) {
+  if ((end - start) <= 1) {
+    return;
+  }
+
+  size_t half = (end - start) / 2;
+
+  vector<const char *> left(start, start + half);
+  vector<const char *> right(start + half, end);
+
+  merge_sort_concurrent(left.data(), left.data() + left.size());
+  merge_sort_concurrent(right.data(), right.data() + right.size());
+
+  merge(start, left.data(), left.data() + left.size(), right.data(),
+        right.data() + right.size());
 }
 
 int main(int argc, const char **argv) {
@@ -69,11 +97,10 @@ int main(int argc, const char **argv) {
   }
   in.close();
 
-  vector<const char *> sorted =
-      merge_sort(lines.data(), lines.data() + lines.size());
+  merge_sort_parallel(lines.data(), lines.data() + lines.size());
 
   ofstream out(output);
-  for (const auto &str : sorted) {
+  for (const auto &str : lines) {
     out << str << '\n';
   }
   out.close();
