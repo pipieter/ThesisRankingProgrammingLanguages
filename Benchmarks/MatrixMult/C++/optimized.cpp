f3c04a8be8c578f299c7cb6a3a3ffeeb351e2f97
Pim Pieters <pim.pieters@ugent.be>
2025-02-24 19:37:01 +0100

    Add BLAS for C++ matrix mult

--- Benchmarks/MatrixMult/C++/optimized.cpp
+++ Benchmarks/MatrixMult/C++/optimized.cpp
@@ -2,32 +2,73 @@
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 
-#include "matrix.hpp"
+#include <cblas.h>
+#include <iomanip>
 
-void matrixmultiply(double **__restrict a, double **__restrict b,
-                    double **__restrict c, size_t n) {
-  assert(n % BLOCK_SIZE == 0);
+class Matrix {
+public:
+  double *values;
+  size_t n;
 
-  for (size_t kk = 0; kk < n; kk += BLOCK_SIZE) {
-    for (size_t jj = 0; jj < n; jj += BLOCK_SIZE) {
-      for (size_t i = 0; i < n; i++) {
-        for (size_t j = jj; j < jj + BLOCK_SIZE; j++) {
-          for (size_t k = kk; k < kk + BLOCK_SIZE; k++) {
-            c[i][j] += a[i][k] * b[k][j];
-          }
-        }
-      }
-    }
+  Matrix(size_t n) {
+    this->n = n;
+
+    values = new double[n * n];
+    std::memset(values, 0, sizeof(double) * n * n);
   }
+
+  ~Matrix() { delete[] values; }
+};
+
+static Matrix read(const char *path) {
+  std::ifstream file(path);
+  std::string line;
+
+  std::getline(file, line);
+  size_t n = std::atoi(line.c_str());
+
+  Matrix matrix(n);
+
+  size_t index = 0;
+  while (std::getline(file, line)) {
+    double value = std::atof(line.c_str());
+    matrix.values[index] = value;
+    index++;
+  }
+
+  return matrix;
+}
+
+static void write(const Matrix &matrix, const char *path) {
+  std::ofstream file(path);
+  file << std::setprecision(8);
+  file << matrix.n << '\n';
+
+  for (size_t i = 0; i < matrix.n * matrix.n; i++) {
+    file << matrix.values[i] << '\n';
+  }
+
+  file.close();
+}
+
+Matrix operator*(const Matrix &a, const Matrix &b) {
+  assert(a.n == b.n);
+
+  size_t n = a.n;
+  Matrix c = Matrix(n);
+
+  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, a.values,
+              n, b.values, n, 1.0, c.values, n);
+
+  return c;
 }
 
 int main(int, const char **argv) {
   Matrix a = read(argv[1]);
   Matrix b = read(argv[2]);
-  Matrix c = Matrix(a.n);
-
-  matrixmultiply(a.values, b.values, c.values, a.n);
+  Matrix c = a * b;
 
   write(c, argv[3]);
 
