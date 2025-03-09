f46f5565329236a83ac05fe32cbc70c0c05fbbac
Pim Pieters <pim.pieters@ugent.be>
2025-03-09 16:53:01 +0100

    Optimize Python optimized MatrixMult

--- Benchmarks/MatrixMult/Python/optimized.py
+++ Benchmarks/MatrixMult/Python/optimized.py
@@ -1,54 +1,34 @@
 import sys
-from typing import List
 import numpy as np
 
 
-class Matrix(object):
-    values: np.ndarray
-    n: int
-
-    def __init__(self, n: int) -> None:
-        self.n = n
-        self.values = np.ndarray((n, n))
-
-
-def read(path: str) -> Matrix:
+def read(path: str) -> np.ndarray:
     file = open(path, "r")
-
-    n = int(file.readline())
-    matrix = Matrix(n)
-
-    index = 0
-    line = file.readline()
-    while line != "":
-        row = index // n
-        col = index % n
-        matrix.values[row, col] = float(line)
-        line = file.readline()
-        index += 1
-
+    lines = file.readlines()
     file.close()
 
-    return matrix
+    n = int(lines[0])
+    values = [float(value) for value in lines[1:]]
+    values = np.asarray(values, dtype=np.float32)
+    values = np.reshape(values, (n, n))
+
+    return values
 
 
-def write(matrix: Matrix, path: str) -> None:
+def write(matrix: np.ndarray, path: str) -> None:
     file = open(path, "w")
 
-    file.write(f"{matrix.n}\n")
-
-    for i in range(matrix.n):
-        for j in range(matrix.n):
-            file.write(f"{matrix.values[i,j]}\n")
+    file.write(f"{matrix.shape[0]}\n")
+    matrix = np.reshape(matrix, matrix.shape[0] * matrix.shape[1])
+    values = map(str, matrix)
+    file.write("\n".join(values))
+    file.write("\n")
 
     file.close()
 
 
-def multiply(a: Matrix, b: Matrix) -> Matrix:
-    assert a.n == b.n
-    matrix = Matrix(a.n)
-    matrix.values = np.asmatrix(a.values) * np.asmatrix(b.values)
-    return matrix
+def multiply(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    return a @ b
 
 
 if __name__ == "__main__":
