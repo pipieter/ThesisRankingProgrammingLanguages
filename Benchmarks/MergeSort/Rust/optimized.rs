83ba099aa7189b897655eb89bb01611330c96017
Pim Pieters <pim@pieters.tv>
2025-04-10 02:52:48 +0200

    Optimize Rust merge sort

--- Benchmarks/MergeSort/Rust/optimized.rs
+++ Benchmarks/MergeSort/Rust/optimized.rs
@@ -6,35 +6,34 @@ use std::{
 
 const CONCURRENT_THRESHOLD: usize = 10_000usize;
 
-fn merge_sort_parallel(entries: &mut [String]) {
+fn merge_sort_parallel(entries: &mut [&String]) {
     if entries.len() < CONCURRENT_THRESHOLD {
         merge_sort_concurrent(entries);
         return;
     }
 
     let half = entries.len() / 2;
-    let mut left: Vec<String> = entries[0..half].to_vec();
-    let mut right: Vec<String> = entries[half..entries.len()].to_vec();
+    let mut left = entries[0..half].to_vec();
+    let mut right = entries[half..entries.len()].to_vec();
 
-    let left_thread = thread::spawn(move || {
-        merge_sort_parallel(&mut left);
-        left
+    thread::scope(|s| {
+        let left_thread = s.spawn(|| {
+            merge_sort_parallel(&mut left);
+        });
+        merge_sort_parallel(&mut right);
+        left_thread.join().unwrap();
     });
-    merge_sort_parallel(&mut right);
-
-    let left = left_thread.join().unwrap();
-
     merge(&left, &right, entries);
 }
 
-fn merge_sort_concurrent(entries: &mut [String]) {
+fn merge_sort_concurrent(entries: &mut [&String]) {
     if entries.len() <= 1 {
         return;
     }
 
     let half = entries.len() / 2;
-    let mut left: Vec<String> = entries[0..half].to_vec();
-    let mut right: Vec<String> = entries[half..entries.len()].to_vec();
+    let mut left = entries[0..half].to_vec();
+    let mut right = entries[half..entries.len()].to_vec();
 
     merge_sort_concurrent(&mut left);
     merge_sort_concurrent(&mut right);
@@ -42,29 +41,29 @@ fn merge_sort_concurrent(entries: &mut [String]) {
     merge(&left, &right, entries);
 }
 
-fn merge(a: &[String], b: &[String], destination: &mut [String]) {
+fn merge<'a>(a: &[&'a String], b: &[&'a String], destination: &mut [&'a String]) {
     let mut i = 0;
     let mut ia = 0;
     let mut ib = 0;
 
     while ia < a.len() && ib < b.len() {
         if a[ia] < b[ib] {
-            destination[i] = a[ia].clone();
+            destination[i] = a[ia];
             ia += 1;
         } else {
-            destination[i] = b[ib].clone();
+            destination[i] = b[ib];
             ib += 1;
         }
         i += 1;
     }
 
     while ia < a.len() {
-        destination[i] = a[ia].clone();
+        destination[i] = a[ia];
         ia += 1;
         i += 1;
     }
     while ib < b.len() {
-        destination[i] = b[ib].clone();
+        destination[i] = b[ib];
         ib += 1;
         i += 1;
     }
@@ -78,12 +77,13 @@ fn main() {
 
     let file = File::open(input).expect("no such file");
     let buf = BufReader::new(file);
-    let mut lines: Vec<String> = buf
+    let lines: Vec<String> = buf
         .lines()
         .map(|l| l.expect("Could not parse line"))
         .collect();
+    let mut str_lines: Vec<&String> = lines.iter().map(|v| v).collect();
 
-    merge_sort_parallel(&mut lines);
+    merge_sort_parallel(&mut str_lines);
 
     let mut out = BufWriter::new(File::create(&output).unwrap());
     for line in lines {
